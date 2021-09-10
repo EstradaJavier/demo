99dfefd85166da966bc2e592bc03dc6d29b95821
javierestrada <estrada.javier.525@gmail.com>
2021-09-10 13:32:24 -0500

    feat: Adding dependency injection to my class

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package com.example.demo.customer;
 
-import com.example.demo.DemoApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,10 @@ import java.util.List;
 @RestController
 public class CustomerController {
 
+//  ----------  Adding the word 'final' makes customerService immutable.
     private final CustomerService customerService;
 
+//  ----------  Adding dependency injection with @Autowired injects a customer service.
     @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,11 +1,12 @@
 package com.example.demo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+//  ----------  Adding @Component here, creates a bean that we can
+//  ----------  injected into many different classes.
 @Component
 public class CustomerService {
 
